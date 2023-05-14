0a1826d7078613046d9a8d3eac2b7fc7fbd568e6
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-05-14 16:32:09 +0530

    10.5 Auth form + validation

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -35,5 +36,10 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form);
+    
   }
 }
